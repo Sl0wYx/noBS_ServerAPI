3e1af223f9879dcf3c1ccb3a8b4c88a32bd9aa7b
Sl0wYx <lol-waring14@mail.ru>
2026-03-23 00:14:27 +0000

    Player name for specific stat return added

---
+++ stats.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+import csv
+
+router = APIRouter()
+
+@router.get('/stats/{uuid}/{stat_name}', tags=['stats'])
+def get_player_stat_by_name(uuid: str, stat_name: str):
+    with open('stats.csv', 'r', newline='') as stats_file:
+        reader = csv.DictReader(stats_file)
+        for row in reader:
+            for col in row:
+                if row['uuid'] == uuid and col == stat_name:
+                    return {"uuid" : row['uuid'], "player_name" : row['Player Name'], "stat_value" : row[col]}
+
+        return {"Error": "Either account with that UUID does not exist or stat name is wrong"}
+
+@router.get('/stats/{uuid}', tags=['stats'])
+def get_all_player_stats(uuid: str):
+    with open('stats.csv', 'r', newline='') as stats_file:
+        reader = csv.DictReader(stats_file)
+        for row in reader:
+            if row['uuid'] == uuid:
+                return row
+
+        return {"Error": "Account with that UUID does not exist"}
