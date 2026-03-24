2e86f115dec4080e729b5d79d71eeb3e312c1269
Sl0wYx <lol-waring14@mail.ru>
2026-03-24 00:51:23 +0000

    Fixed paths

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends, FastAPI
-from routers import accounts
-from routers import stats
-from routers import online
-app = FastAPI()
-
-app.include_router(accounts.router)
-app.include_router(stats.router)
-app.include_router(online.router)
