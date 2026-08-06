18b2b46534c92fb80f4ac04a06d858310a3858de
Sl0wYx <lol-waring14@mail.ru>
2026-08-06 19:14:08 +0100

    Update relative path to absolute in telegram.py

--- app/routers/telegram.py
+++ app/routers/telegram.py
@@ -1,11 +1,17 @@
+from idlelib import __main__
+
 from fastapi import APIRouter, Request, HTTPException
+from app.core import common
 import json
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 from fastapi.responses import FileResponse
 
-load_dotenv(Path("app/data/private/.env"))
+MSG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/message.json"))
+IMG_FOLDER_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/images/"))
+ENV_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/private/.env"))
+load_dotenv(ENV_PATH)
 
 API_TOKEN = os.getenv("API_TOKEN")
 API_URL = os.getenv("API_URL")
@@ -15,17 +21,16 @@ router = APIRouter()
 @router.get("/get_image/{date}", tags=["telegram"])
 def get_image(date : str):
     date_str = str(date.replace(" ", "_").replace(":", "-"))
-    local_url = Path(f"app/data/images/{date_str}.png")
+    local_url = Path(os.path.join(IMG_FOLDER_PATH, f"{date_str}.png"))
 
     if not local_url.exists():
         raise HTTPException(status_code=404, detail="Image not found")
-
     return FileResponse(local_url)
 
 @router.get("/get_message", tags=["telegram"])
 def get_message():
     try:
-        with open("app/data/message.json", mode='r', encoding='utf-8-sig') as message:
+        with open(MSG_PATH, mode='r', encoding='utf-8-sig') as message:
             return json.load(message)
     except (FileNotFoundError, IOError):
         raise HTTPException(status_code=500, detail="File message is unreachable")
@@ -35,7 +40,7 @@ def receive_message(data: dict, request: Request):
     if request.headers.get("authorization") != API_TOKEN:
         raise HTTPException(status_code=401, detail="Invalid API Token")
     else:
-        base = Path("app/data")
+        base = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/"))
         path = base / "message.json"
 
         if not base.exists():
@@ -50,4 +55,3 @@ def receive_message(data: dict, request: Request):
             path.write_text(json.dumps(data))
         except IOError:
             raise HTTPException(status_code=500, detail="Failed to write message")
-        
