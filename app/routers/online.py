39cbd9099e13d4108c6aa37026ff5c6776241f6c
Sl0wYx <lol-waring14@mail.ru>
2026-08-10 04:28:50 +0100

    Fix global variables never assigned

--- app/routers/online.py
+++ app/routers/online.py
@@ -3,8 +3,6 @@ from app.core import common
 import json
 import os
 
-from core.common import load_file
-
 PATH_JSON = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/stats.json"))
 PATH_CSV = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/stats.csv"))
 router = APIRouter()
@@ -13,8 +11,11 @@ csv_stats = common.load_file(PATH_CSV, "csv")
 csv_last_change = common.file_last_change(PATH_CSV)
 def load_online():
     global csv_stats, csv_last_change
+
     current_csv_change = common.file_last_change(PATH_CSV)
-    stats = common.load_file(PATH_CSV, "csv") if csv_last_change != current_csv_change else csv_stats
+    if csv_last_change != current_csv_change:
+        csv_stats = common.load_file(PATH_CSV, "csv")
+        csv_last_change = current_csv_change
 
     try:
         with open(PATH_JSON, 'r', encoding='utf-8') as online_file:
@@ -24,7 +25,7 @@ def load_online():
 
             for player_name in player_names:
                 try:
-                    player = common.get_all_player_stats_by_name(stats)
+                    player = common.get_all_player_stats_by_name(csv_stats, player_name)
                     if player and ('uuid' in player):
                         res.append(player['uuid'])
                 except Exception as e:
